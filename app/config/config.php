2bb8a10682515842524dd5828c8c4a482040f8ce
Octyo Paswa Putra <amateur.fullstackdev@gmail.com>
2023-05-28 07:39:16 +0700

    chore:[minor] change something

--- app/config/config.php
+++ app/config/config.php
@@ -5,5 +5,5 @@ define('BASEURL', 'http://ternakita.localhost');
 // Database Configuration
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
-define('DB_PASSWORD', 'binary1010');
+define('DB_PASSWORD', '');
 define('DB_NAME', 'ternakita_db');
